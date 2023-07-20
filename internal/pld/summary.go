0e4ea878ffa5a9c852214d50d2a7708259d6d918
Chouteau Perry <perry.chouteau@epitech.eu>
2023-07-20 12:12:54 +0200

    merge: summary

--- internal/pld/summary.go
+++ internal/pld/summary.go
@@ -5,8 +5,10 @@ import "strconv"
 func (cli *Client) AddSummary(schema int, deliveriesCard int, sectors map[string]int, UserStories map[string]int, advanceReport int) {
 	var y float64
 	tr := cli.UnicodeTranslatorFromDescriptor("")
+	currentPage := cli.Pdf.PageNo()
 
 	cli.AddPage()
+	currentPage++
 
 	cli.Pdf.SetTextColor(0, 0, 0) //black
 	cli.Pdf.SetFont("Arial", "B", 10)
@@ -15,14 +17,14 @@ func (cli *Client) AddSummary(schema int, deliveriesCard int, sectors map[string
 	cli.Pdf.SetX((cli.Width - cli.CardWith) / 2)
 	cli.Pdf.MultiCell(cli.CardWith-20, 7, "Sommaire", "1", "", false)
 	cli.Pdf.SetXY(((cli.Width-cli.CardWith)/2)+cli.CardWith-20, y)
-	cli.Pdf.MultiCell(cli.CardWith-20, 7, strconv.Itoa(cli.Pdf.PageNo()), "1", "", false)
+	cli.Pdf.MultiCell(cli.CardWith-20, 7, strconv.Itoa(currentPage), "1", "", false)
 
 	// //Shéma fonctionnel
 	y = cli.Pdf.GetY()
 	cli.Pdf.SetX((cli.Width - cli.CardWith) / 2)
 	cli.Pdf.MultiCell(cli.CardWith-20, 7, tr("1. Schéma Fonctionnel"), "1", "", false)
 	cli.Pdf.SetXY(((cli.Width-cli.CardWith)/2)+cli.CardWith-20, y)
-	cli.Pdf.MultiCell(cli.CardWith-20, 7, strconv.Itoa(cli.Pdf.PageNo()), "1", "", false)
+	cli.Pdf.MultiCell(cli.CardWith-20, 7, strconv.Itoa(currentPage+1), "1", "", false)
 
 	// cli.Pdf.SetX((cli.Width - cli.CardWith) / 2)
 	// cli.Pdf.MultiCell(cli.CardWith, 7, tr("1. Schéma Fonctionnel"), "1", "", false)
